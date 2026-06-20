68e8f6c9044f7ab613701a007500ea29b13f191f
Shazia <shaziaahmad528@gmail.com>
2026-06-20 18:17:54 +0400

    added a project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,6 +4,9 @@ import ProjectBox from "../components/ProjectBox";
 import weatherapp from "../assets/projects/weatherapp.png";
 import feelsync from "../assets/projects/Screenshot 2026-06-09 175225.png"
 import quiz from "../assets/projects/quiz.png"
+import csp from "../assets/csp.png"
+import Portfolio from "../assets/projects/portfolio.png"
+
 export default function Projects () {
     return(
         <div className="mainpage">
@@ -29,7 +32,11 @@ export default function Projects () {
                         <ProjectBox imageSrc={feelsync} projectName="FeelSync Mood playlist generator" completed="fifty" descrip="FeelSync is a web application that generates personalized recommendations based on a user's mood. Enter how you're feeling, and the app generates music playlists, activities, and book recommendations. This project is currently under development." techstack="React, Typescript, Express, Gemini" starsIcon={5}/>
                     </a>
                     <a href="https://github.com/Shazia-001/Portfolio">
-                        <ProjectBox imageSrc="" projectName="Personal Portfolio" completed="hundred" descrip="A personal portfolio website showcasing my projects and technical skills as a full-stack developer. Built with a focus on responsive design, performance, and user experience." techstack="React, Typescript, EmailJS" starsIcon={5}/>
+                        <ProjectBox imageSrc={Portfolio} projectName="Personal Portfolio" completed="hundred" descrip="A personal portfolio website showcasing my projects and technical skills as a full-stack developer. Built with a focus on responsive design, performance, and user experience." techstack="React, Typescript, EmailJS" starsIcon={5}/>
+                    </a>
+
+                    <a href="">
+                        <ProjectBox imageSrc={csp} projectName="Corporate Service Website" completed="eighty" descrip="A proffesional website for a CSP company in the UAE for a newly formed company. This is currently under process." techstack="React, Typescript, EmailJS" starsIcon={5}/>
                     </a>
                     
 
